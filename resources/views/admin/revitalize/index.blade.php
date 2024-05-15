450f90cd6c7bce91f6006ff0ec5830922f46b3cf
DeskTeam360 <deskteam360@gmail.com>
2024-05-16 05:21:33 +0700

    init revitalize

--- resources/views/admin/revitalize/index.blade.php
+++ resources/views/admin/revitalize/index.blade.php
@@ -2,7 +2,8 @@
     <div class="container full-container py-5">
         <div class="w-full">
 {{--            <a href="{{ route('limit-link.create') }}" class="btn btn-primary">Create limit link</a>--}}
-            <livewire:table.master name="Revitalize"/>
+{{--            <livewire:table.master name="Revitalize"/>--}}
+            <livewire:revitalize/>
         </div>
     </div>
 </x-admin-layout>
