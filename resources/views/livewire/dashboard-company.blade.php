a348a1d5227497e21053bb3b8f1ff4e5b211402f
DeskTeam-360 <deskteam360@gmail.com>
2024-07-12 20:08:37 +0700

    test 1 gf

--- resources/views/livewire/dashboard-company.blade.php
+++ resources/views/livewire/dashboard-company.blade.php
@@ -178,7 +178,7 @@ $q->where('meta_key','company')
                             {{ $c->title }}
                         </td>
                         <td class=" whitespace-nowrap  dark:text-darklink p-2 text-center">
-                            {{ CompanyEmployee::where('company_id',$c->id)->count() }}
+                            {{ \App\Models\CompanyEmployee::where('company_id',$c->id)->count() }}
                         </td>
                         <td class="p-2 whitespace-nowrap text-center">
                             {{ $c->created_at->format('F d, Y') }}
@@ -203,29 +203,29 @@ $q->where('meta_key','company')
 
 
 
-    @php($pageTitle = ['Revitalize', 'Transform','Sustain'])
-    @foreach($pageTitle as $pt)
-        <div class="lg:col-span-4 md:col-span-6 sm:col-span-12 col-span-12">
-            <div class="card">
-                <div class="card-body flex-row py-4 flex items-center gap-2">
-                    <div class="bg-primary h-10 w-10 p-1 text-center text-white" style="border-radius: 100px">
-                        <i class="ti ti-users text-2xl"></i>
-                    </div>
-                    <div class="">
-                        <h5 class="text-3xl leading-normal">
-                            {{ CourseList::where('course_title',$pt)->count() }}
-                        </h5>
-                        <span class="text-lg flex items-center gap-1">
-                                {{ $pt }}
-                            </span>
-                    </div>
-                    <a class="ms-auto text-2xl" style="border-radius: 40px">
-                        <i class="ti ti-arrow-up-right"></i>
-                    </a>
-                </div>
-            </div>
-        </div>
-    @endforeach
+{{--    @php($pageTitle = ['Revitalize', 'Transform','Sustain'])--}}
+{{--    @foreach($pageTitle as $pt)--}}
+{{--        <div class="lg:col-span-4 md:col-span-6 sm:col-span-12 col-span-12">--}}
+{{--            <div class="card">--}}
+{{--                <div class="card-body flex-row py-4 flex items-center gap-2">--}}
+{{--                    <div class="bg-primary h-10 w-10 p-1 text-center text-white" style="border-radius: 100px">--}}
+{{--                        <i class="ti ti-users text-2xl"></i>--}}
+{{--                    </div>--}}
+{{--                    <div class="">--}}
+{{--                        <h5 class="text-3xl leading-normal">--}}
+{{--                            {{ \App\Models\CourseList::where('course_title',$pt)->count() }}--}}
+{{--                        </h5>--}}
+{{--                        <span class="text-lg flex items-center gap-1">--}}
+{{--                                {{ $pt }}--}}
+{{--                            </span>--}}
+{{--                    </div>--}}
+{{--                    <a class="ms-auto text-2xl" style="border-radius: 40px">--}}
+{{--                        <i class="ti ti-arrow-up-right"></i>--}}
+{{--                    </a>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    @endforeach--}}
 </div>
 
 
